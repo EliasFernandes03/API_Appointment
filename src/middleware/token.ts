f41c26fd5103ca1b303eaecfa13499afb44ee38f
EliasFernandes03 <elias.neto@dunasystem.com>
2024-01-09 16:41:08 -0300

    fix:adjust

--- src/middleware/token.ts
+++ src/middleware/token.ts
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function verificarToken(req: Request, res: Response, next: NextFunction): void | Response<any, Record<string, any>> {
-  const token = req.headers.authorization;
-
+  const token = req.headers.authorization?.split(' ')[1];
   if (!token || token !== process.env.TOKEN) {
     return res.status(401).json({ message: 'Acesso não autorizado.' });
   }
